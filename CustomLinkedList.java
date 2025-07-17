185e995561dcc1f47d8065afada5d1f71a7d1671
JBenitez-ThinkHat <jose.conde@thinkhat.ai>
2025-07-17 11:48:07 -0400

    Added Linked List Code

--- CustomLinkedList.java
+++ CustomLinkedList.java
@@ -1,5 +1,43 @@
-public class CustomLinkedList {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class CustomLinkedList<T> {
+    private LinkedListNode<T> head;
+    private LinkedListNode<T> tail;
+    private int length;
+
+    public LinkedListNode<T> getHead() {
+        return this.head;
     }
+
+    public LinkedListNode<T> getTail() {
+        return this.tail;
+    }
+
+    public T delete(T val) {
+        LinkedListNode<T> cur = this.head;
+
+        
+
+        while (cur != null) {
+            if (cur.val == val) {
+
+            }
+            cur = cur.next;
+        }
+    }
+
+    public String toString() {
+        LinkedListNode<T> cur = this.head;
+        StringBuilder retString = new StringBuilder();
+
+        while (cur != null) {
+            retString.append(cur.val + ", ");
+            cur = cur.next;
+        }
+
+        return retString.toString();
+    }
+}
+
+class LinkedListNode<T> {
+    public T val;
+    public LinkedListNode<T> next;
 }
